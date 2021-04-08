f6264e26d99705dcea9758d9f39c8bfa78ca6ba6
MateusDeBueno <mateusbueno96@gmail.com>
2021-04-07 21:29:29 -0300

    initial

---
+++ Peripheral_Setup.c
@@ -0,0 +1,197 @@
+/*
+ * Peripheral_Setup.c
+ *
+ *  Created on: 7 de abr de 2021
+ *      Author: Mateus
+ */
+
+
+#include "peripheral_setup.h"
+
+
+void Setup_GPIO(void){
+    EALLOW;
+
+    /*
+    ao escolher um GPIO, deve-se olhar em qual MUX ele participa, varia de A ate F
+    GpioCtrlRegs.GPxMUXy.bit.GPIOz = 0;
+    x = A,B,C...
+    y = 1,2
+    z = 0...15, 16...31 (A)
+    z = 32...47, 48...63 (B)
+    z = 64...79, 80...95 (C)
+    z = 96...111, 112...127(D)
+    z = 128...143, 144...159(E)
+    z = 160...168 (F)
+    */
+
+    //GPIO31 - LED
+    GpioCtrlRegs.GPAGMUX2.bit.GPIO31 = 0;
+    GpioCtrlRegs.GPAMUX2.bit.GPIO31 = 0;
+    GpioCtrlRegs.GPADIR.bit.GPIO31 = 1;     //configura como saida, so é necessario se for um uso generico, feito GPIO
+    GpioCtrlRegs.GPAPUD.bit.GPIO31 = 1;     //adiciona pull-down
+
+    //GPIO34 - LED
+    GpioCtrlRegs.GPBGMUX1.bit.GPIO34 = 0;
+    GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+    GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
+    GpioCtrlRegs.GPBPUD.bit.GPIO34 = 1;
+
+    GpioDataRegs.GPBSET.bit.GPIO34 = 1; //apaga leds chatos, eh com logica reversa
+    GpioDataRegs.GPASET.bit.GPIO31 = 1;
+
+
+    //PWM1A
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO0 = 0;    //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 1;     //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPAPUD.bit.GPIO0 = 1;      //desabilita pull up
+
+    //PWM1B
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO1 = 0;    //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 1;     //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPAPUD.bit.GPIO1 = 1;      //desabilita pull up
+
+    //PWM7A
+    GpioCtrlRegs.GPEGMUX2.bit.GPIO157 = 0;    //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPEMUX2.bit.GPIO157 = 1;     //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPEPUD.bit.GPIO157 = 1;      //desabilita pull up
+
+    //PWM8A
+    GpioCtrlRegs.GPEGMUX2.bit.GPIO159 = 0;    //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPEMUX2.bit.GPIO159 = 1;     //configura como PWM, escolhe a funcao
+    GpioCtrlRegs.GPEPUD.bit.GPIO159 = 1;      //desabilita pull up
+
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO14 = 0;
+    GpioCtrlRegs.GPAMUX1.bit.GPIO14 = 0;
+    GpioCtrlRegs.GPADIR.bit.GPIO14 = 1;
+    GpioCtrlRegs.GPAPUD.bit.GPIO14 = 1;
+
+
+    EDIS;
+}
+
+
+void Setup_ePWM(void){
+    EALLOW;
+    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 0;       //desativo o pwm para poder configurar
+
+    /////////////CONFIGURA PWM1/////////////
+
+    //configura portadora
+    EPwm1Regs.TBPRD = 2000;                             //25kz //PRD=MCclock/(2*fs), porem se updown PRD = MCclock/(4*fs)
+    EPwm1Regs.TBPHS.bit.TBPHS = 0;                      //defasagem 0
+    EPwm1Regs.TBCTL.bit.SYNCOSEL = TB_CTR_ZERO;         //quando igual a zero gera pulso de referencia pra outros PWM
+    EPwm1Regs.TBCTR = 0x0000;
+    EPwm1Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;      //count up/down
+    EPwm1Regs.TBCTL.bit.PHSEN = TB_DISABLE;             //desabilitar deslocamente de fase
+    EPwm1Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1;            //pre scale do clock do pwm, util para baixas frequencias, para o TBPRD n estourar o valor maximo
+    EPwm1Regs.TBCTL.bit.CLKDIV = TB_DIV1;               //TBPRD eh unit16, entao TBPRD vale entre 0 ate 65535
+
+    //configura shadow (comparadores so atualizam apos um evento)
+    EPwm1Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;         //habilita shadow
+    EPwm1Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO_PRD;   //atualiza comparador A somente no ZERO e no PRD
+    EPwm1Regs.CMPCTL.bit.SHDWBMODE = CC_SHADOW;         //habilita shadow
+    EPwm1Regs.CMPCTL.bit.LOADBMODE = CC_CTR_ZERO_PRD;   //atualiza comparador B somente no ZERO e no PRD
+
+    //configura as ações para PWM1A
+    EPwm1Regs.AQCTLA.bit.PRD = AQ_NO_ACTION;
+    EPwm1Regs.AQCTLA.bit.ZRO = AQ_NO_ACTION;
+    EPwm1Regs.AQCTLA.bit.CAU = AQ_CLEAR;
+    EPwm1Regs.AQCTLA.bit.CAD = AQ_SET;
+
+    EPwm1Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;       //EPWM B é complementar do EPWM A
+    EPwm1Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;  //habilita dead band
+    EPwm1Regs.DBFED.bit.DBFED = 100;                //rising edges
+    EPwm1Regs.DBRED.bit.DBRED = 100;                //falling edges
+
+    //Trigger ADC
+    EPwm1Regs.ETSEL.bit.SOCAEN = 1;                  //enable SOC on A group
+    EPwm1Regs.ETSEL.bit.SOCASEL = ET_CTR_PRDZERO;   //dispara ADC no vale e no pico da triangular
+    EPwm1Regs.ETPS.bit.SOCAPRD = ET_1ST;            //trigger on every event
+
+
+    EPwm1Regs.CMPA.bit.CMPA = EPwm1Regs.TBPRD/2;
+
+
+    /////////////CONFIGURA PWM7/////////////
+    EPwm7Regs.TBPRD = 2000;                             //25kz //PRD=MCclock/(2*fs), porem se updown PRD = MCclock/(4*fs)
+    EPwm7Regs.CMPA.bit.CMPA = 0;
+    EPwm7Regs.TBPHS.bit.TBPHS = 0;                      //defasagem 0
+    EPwm7Regs.TBCTL.bit.SYNCOSEL = TB_SYNC_IN;          //recebe pulso de sincronismo
+    EPwm7Regs.TBCTR = 0x0000;
+    EPwm7Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;      //count up/down
+    EPwm7Regs.TBCTL.bit.PHSEN = TB_ENABLE;              //habilita deslocamente de fase
+    EPwm7Regs.TBCTL.bit.PHSDIR = TB_DOWN;               //configura se a fase avanca ou atrasa
+    EPwm7Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1;            //pre scale do clock do pwm, util para baixas frequencias, para o TBPRD n estourar o valor maximo
+    EPwm7Regs.TBCTL.bit.CLKDIV = TB_DIV1;               //TBPRD eh unit16, entao TBPRD vale entre 0 ate 65535
+
+    //configura shadow (comparadores so atualizam apos um evento)
+    EPwm7Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;         //habilita shadow
+    EPwm7Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO_PRD;   //atualiza comparador A somente no ZERO e no PRD
+
+    //configura as ações para PWM1A
+    EPwm7Regs.AQCTLA.bit.PRD = AQ_NO_ACTION;
+    EPwm7Regs.AQCTLA.bit.ZRO = AQ_NO_ACTION;
+    EPwm7Regs.AQCTLA.bit.CAU = AQ_CLEAR;
+    EPwm7Regs.AQCTLA.bit.CAD = AQ_SET;
+
+
+    /////////////CONFIGURA PWM8/////////////
+    EPwm8Regs.TBPRD = 2000;                             //25kz //PRD=MCclock/(2*fs), porem se updown PRD = MCclock/(4*fs)
+    EPwm8Regs.CMPA.bit.CMPA = 0;
+    EPwm8Regs.TBPHS.bit.TBPHS = 0;                      //defasagem 0
+    EPwm8Regs.TBCTL.bit.SYNCOSEL = TB_SYNC_IN;          //recebe pulso de sincronismo
+    EPwm8Regs.TBCTR = 0x0000;
+    EPwm8Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;      //count up/down
+    EPwm8Regs.TBCTL.bit.PHSEN = TB_ENABLE;              //habilita deslocamente de fase
+    EPwm8Regs.TBCTL.bit.PHSDIR = TB_DOWN;               //configura se a fase avanca ou atrasa
+    EPwm8Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1;            //pre scale do clock do pwm, util para baixas frequencias, para o TBPRD n estourar o valor maximo
+    EPwm8Regs.TBCTL.bit.CLKDIV = TB_DIV1;               //TBPRD eh unit16, entao TBPRD vale entre 0 ate 65535
+
+    //configura shadow (comparadores so atualizam apos um evento)
+    EPwm8Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;         //habilita shadow
+    EPwm8Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO_PRD;   //atualiza comparador A somente no ZERO e no PRD
+
+    //configura as ações para PWM1A
+    EPwm8Regs.AQCTLA.bit.PRD = AQ_NO_ACTION;
+    EPwm8Regs.AQCTLA.bit.ZRO = AQ_NO_ACTION;
+    EPwm8Regs.AQCTLA.bit.CAU = AQ_CLEAR;
+    EPwm8Regs.AQCTLA.bit.CAD = AQ_SET;
+
+    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 1;       //ativa o pwm
+    EDIS;
+}
+
+
+
+
+void Setup_ADC(void){
+    Uint16 acqps;
+    //determine minimum acquisition window (in SYSCLKS) based on resolution
+    if (ADC_RESOLUTION_12BIT == AdcaRegs.ADCCTL2.bit.RESOLUTION)
+        acqps = 14;             //aproximadamente 75ns
+    else                        //16 bits de resolucao
+        acqps = 63;             //aproximadamente 320ns
+
+    EALLOW;
+    AdcaRegs.ADCCTL2.bit.PRESCALE = 6;                      //seta para ADCCLK divitor por 4
+    AdcSetMode(ADC_ADCA, ADC_RESOLUTION_12BIT, ADC_SIGNALMODE_SINGLE);
+    AdcaRegs.ADCCTL1.bit.INTPULSEPOS = 1;                   //seta pulso um ciclo antes do resultado
+    AdcaRegs.ADCCTL1.bit.ADCPWDNZ =1;                       //liga o ADC
+    DELAY_US(1000);                                         //delay de 1ms pra permitir q os ADC ligem
+
+    AdcaRegs.ADCSOC0CTL.bit.CHSEL = 3;                      //escolhe canal a ser amostrado
+    AdcaRegs.ADCSOC0CTL.bit.ACQPS = acqps;                  //sample window is 15 SYSCLK cycles
+    AdcaRegs.ADCSOC0CTL.bit.TRIGSEL = TRIG_SEL_ePWM1_SOCA;  //trigger on ePWM1 SOCA
+
+    AdcaRegs.ADCSOC1CTL.bit.CHSEL = 4;                      //escolhe canal a ser amostrado
+    AdcaRegs.ADCSOC1CTL.bit.ACQPS = acqps;                  //sample window is 15 SYSCLK cycles
+    AdcaRegs.ADCSOC1CTL.bit.TRIGSEL = TRIG_SEL_ePWM1_SOCA;  //trigger on ePWM1 SOCA
+
+    AdcaRegs.ADCINTSEL1N2.bit.INT1SEL = 0x01;               //end of SOC1 will set INT1 flag
+    AdcaRegs.ADCINTSEL1N2.bit.INT1E = 1;                    //enable INT1 flag
+    AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;                  //make sure INT1 flag is cleared
+
+    EDIS;
+
+}
---
+++ main.c
@@ -0,0 +1,90 @@
+#include "peripheral_setup.h"
+
+/**
+ * main.c
+ */
+
+
+uint32_t count=0, index=0;
+uint16_t sinetable[125];
+
+uint16_t adc1 = 0;
+uint16_t adc2 = 0;
+
+uint16_t plot[400];
+uint16_t *adc = &adc1;
+
+uint16_t deleta;
+
+
+__interrupt void isr_cpu_timer0(void);
+__interrupt void isr_adc(void);
+
+int main(void)
+{
+    InitSysCtrl();                  //Initialize System Control
+    DINT;                           //Disable CPU interrupts
+    InitPieCtrl();                  //Initialize the PIE control register to their default state
+    IER = 0x0000;                   //Disable CPU interrupts
+    IFR = 0x0000;                   //Clear all CPU interrupt flags
+    InitPieVectTable();             //Initialize the PIE vector table
+
+    Setup_GPIO();
+    Setup_ePWM();
+    Setup_ADC();
+
+    EALLOW;
+    PieVectTable.TIMER0_INT = &isr_cpu_timer0;  //atribui endereco para funcao interrupt timer0
+    PieVectTable.ADCA1_INT = &isr_adc;
+    PieCtrlRegs.PIEIER1.bit.INTx7 = 1;          //timer0 //coluna 7 da tabela 'PIE Group 1 Vectors - Muxed into CPU INT1'
+    PieCtrlRegs.PIEIER1.bit.INTx1 = 1;          //ADC //coluna 1 da mesma linha que o timer
+    EDIS;
+    IER |= M_INT1;  //como esta na mesma linha q o timer, n precisa habilitar outra linha
+
+    InitCpuTimers(); //inicializa timer
+    ConfigCpuTimer(&CpuTimer0, 200, 160); //configura o timer (enredeco, clock in Mega, periodo em Micro)
+    CpuTimer0Regs.TCR.all = 0x4001; //importante setar o bit 14, ele habilita o timer
+
+    for (index = 0; index < 125; index++){
+        sinetable[index] = (uint16_t) (1000.0*(1.0 + sin(6.28318531/125.0*((float)index))));
+    }
+    index = 0;
+
+    EINT;                           //Enable Global interrupt INTM
+    ERTM;                           //Enable Global realtime interrupt DBGM
+
+    while(1){
+        //
+    }
+}
+
+__interrupt void isr_cpu_timer0(void){
+    GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;
+
+
+    index = (index == 124) ? 0 : (index+1);
+
+    EPwm7Regs.CMPA.bit.CMPA = sinetable[index];
+    EPwm8Regs.CMPA.bit.CMPA = sinetable[index];
+
+
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1; //flag q sinaliza fim da interrupt, n apagar
+}
+
+
+__interrupt void isr_adc(void){
+    GpioDataRegs.GPADAT.bit.GPIO14 = 1;
+
+    adc1 = AdcaResultRegs.ADCRESULT0;
+    adc2 = AdcaResultRegs.ADCRESULT1;
+
+
+
+    //plot[index] = *adc;
+
+    deleta++;
+    AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;  //clear INT1 flag
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+
+    GpioDataRegs.GPADAT.bit.GPIO14 = 0;
+}
